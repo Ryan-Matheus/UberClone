3fcf2041be77980a45bf3f593a61bec8bb6c7ba6
Ryan-Matheus <ryanpqt266@gmail.com>
2022-03-28 12:03:16 -0300

    HomeController add

---
+++ Controller/HomeController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeController.swift
+//  UberClone
+//
+//  Created by user on 28/03/22.
+//
+
+import UIKit
+import Firebase
+
+class HomeController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkIfUserIsLoggedIn()
+        
+        
+        view.backgroundColor = .red
+    }
+    
+    //MARK: - API
+    
+    func checkIfUserIsLoggedIn() {
+        if Auth.auth().currentUser?.uid == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else {
+            print("DEBUG: User id is \(Auth.auth().currentUser?.uid)")
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("DEBUG: Error signing out")
+        }
+    }
+}
--- Controller/LoginController.swift
+++ Controller/LoginController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginController: UIViewController {
     
@@ -44,7 +45,7 @@ class LoginController: UIViewController {
         let button = AuthButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        button.
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
     
@@ -70,6 +71,20 @@ class LoginController: UIViewController {
    
     
     // MARK: - Selectors
+    
+    @objc func handleLogin() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else {return}
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("fail to log user in with error \(error.localizedDescription)")
+                return
+            }
+            
+            print("succesfully logged in")
+        }
+    }
     
     @objc func handleShowSignUp() {
         let controller = SingUpController()
