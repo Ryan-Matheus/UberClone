226400e42a4b06e5e7123495153fad5e8e04a4c8
Ryan-Matheus <ryanpqt266@gmail.com>
2022-04-01 10:12:36 -0300

    fetching user data

---
+++ Service/Service.swift
@@ -0,0 +1,26 @@
+//
+//  Service.swift
+//  UberClone
+//
+//  Created by user on 31/03/22.
+//
+
+import Firebase
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
+
+struct Service {
+    
+    static let shared = Service()
+    let currentUid = Auth.auth().currentUser?.uid
+    
+    func fetchUserData() {
+        REF_USERS.child(currentUid!).observeSingleEvent(of: .value) { (snapshot) in
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            guard let fullname = dictionary["fullname"] as? String else {return}
+            print("DEBUG: User full name is \(fullname)")
+        }
+        
+    }
+}
